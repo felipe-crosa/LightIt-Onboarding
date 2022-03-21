63de8648e1ccccf7e482ae291ec131dbc5737e23
Felipe Crosa <felipecrosa@Felipes-MacBook-Pro.local>
2022-03-21 17:14:36 -0300

    Composer CS

---
+++ FourthChallenge/FlyAway/tests/Feature/AirlineControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AirlineControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_list_airlines()
+    {
+    }
+}
